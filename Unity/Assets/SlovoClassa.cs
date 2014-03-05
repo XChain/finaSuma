a74214eb0cdaa75d17476f1b9cf2d382f8166bc6
XChain <xs.agita@gmail.com>
2014-03-05 22:46:57 +0100

    neke skripte napravljene nesto >.>

---
+++ Unity/Assets/SlovoClassa.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlovoClassa : MonoBehaviour {
+
+	string _slovo;
+	bool _vidiSe;
+
+	SlovoClassa(char slovo,bool vidiSe)
+	{
+		_slovo = slovo.ToString;
+		//_slovo.c
+	}
+}
